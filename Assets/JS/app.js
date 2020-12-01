d9500c0a3093311c784f9ac20e64ecb137073fe8
jurgen1c <jurgen1c@gmail.com>
2020-12-01 09:59:40 -0600

    Add read status & minor styling

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -12,12 +12,52 @@ function Book(title, author, numpages, read) {
   this.read = read;
 }
 
+
 // Tests -------------------------
 const book1 = new Book('may', 'sunday', '500', 'Not Read');
 const book2 = new Book('december', 'jurgen', '300', 'Read');
 
 library.push(book1);
 library.push(book2);
+
+function showForm() {
+  form.className = 'show';
+}
+
+function showLibrary() {
+  libWrap.innerHTML = '';
+  for (let i = 0; i < library.length; i += 1) {
+    const bookCard = document.createElement('div');
+    const btitle = document.createElement('h2');
+    const asubtitle = document.createElement('h3');
+    const content = document.createElement('p');
+    btitle.innerText = library[i].title;
+    asubtitle.innerText = library[i].author.toUpperCase();
+    content.innerHTML = `Pages: ${library[i].numpages} ${library[i].read}`;
+
+    bookCard.setAttribute('class', 'book-card');
+    btitle.setAttribute('class', 'card-title');
+    asubtitle.setAttribute('class', 'card-author');
+    content.classList = 'card-content';
+
+    const rbtn = document.createElement('button');
+    rbtn.innerHTML = 'Delete Book';
+    rbtn.id = `remove-${i}`;
+    rbtn.className = 'btn';
+    rbtn.setAttribute('onclick', `deleteBook(${i})`);
+    const sbtn = document.createElement('button');
+    sbtn.innerHTML = 'Status';
+    sbtn.id = `status-${i}`;
+    sbtn.className = 'btn';
+    sbtn.setAttribute('onclick', `readStatus(${i})`);
+    bookCard.appendChild(btitle);
+    bookCard.appendChild(asubtitle);
+    bookCard.appendChild(content);
+    bookCard.appendChild(rbtn);
+    bookCard.appendChild(sbtn);
+    libWrap.appendChild(bookCard);
+  }
+}
 
 function createBook() {
   const book = new Book();
@@ -29,40 +69,16 @@ function createBook() {
   showLibrary();
 }
 
-function showForm() {
-  form.className = 'show';
-}
-
-function showLibrary() {
-  while (libWrap.firstChild) {
-    libWrap.removeChild(libWrap.firstChild);
-  } 
-  for (let i = 0; i < library.length; i += 1) {
-    const bookCard = document.createElement('div');
-    const btitle = document.createElement('h2');
-    const asubtitle = document.createElement('h3');
-    const content = document.createElement('p');
-    btitle.innerText = library[i].title;
-    asubtitle.innerText = library[i].author;
-    content.innerHTML = library[i].numpages + library[i].read;
-
-    bookCard.setAttribute('class', 'book-card');
-    btitle.setAttribute('class', 'card-title');
-    asubtitle.setAttribute('class', 'card-author');
-    content.classList = 'card-content';
-
-    let btn = document.createElement('button');
-    btn.innerHTML = 'Delete Book';
-    btn.setAttribute('onclick', `deleteBook(${i})`);
-    bookCard.appendChild(btitle);
-    bookCard.appendChild(asubtitle);
-    bookCard.appendChild(content);
-    bookCard.appendChild(btn);
-    libWrap.appendChild(bookCard);
+function readStatus(b) {
+  if (library[b].read === 'Read') {
+    library[b].read = 'Not Read';
+  } else {
+    library[b].read = 'Read';
   }
+  showLibrary();
 }
 
-function deleteBook(idx){
+function deleteBook(idx) {
   library.splice(idx, 1);
   showLibrary();
 }
